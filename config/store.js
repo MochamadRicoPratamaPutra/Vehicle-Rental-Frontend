5461db693ae485425e5875abe8736d2b16d578c7
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-09-13 20:27:25 +0700

    adding redux devtool

--- config/store.js
+++ config/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
-import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
@@ -10,5 +10,5 @@ const persistConfig = {
   whitelist: ['user'],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
 export const persistor = persistStore(store);
