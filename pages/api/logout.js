f92beeb1148ac829ac82d313c50d30ffced02a53
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-09-17 19:59:14 +0700

    fixing cookie

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,18 +1,10 @@
-import axios from 'axios';
 import cookie from 'cookie';
-const login = (req, res) => {
-  if (req.method === 'POST') {
-    const { email, password } = req.body;
-    const data = {
-      email,
-      password,
-    };
-    console.log(data)
+import axios from 'axios';
+const logout = (req, res) => {
+  if (req.method === 'GET') {
     axios
-      .post(`${process.env.REACT_APP_API_URL}/users/login`, data)
-      .then((response) => {
-        console.log(response, ' response login');
-        const result = response.data.data;
+      .get(`${process.env.REACT_APP_API_URL}/users/login`, data)
+      .then(() => {
         res.setHeader('Access-Control-Allow-Headers', '*');
         res.setHeader('Access-Control-Allow-Credentials', true);
         res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -21,30 +13,31 @@ const login = (req, res) => {
             // httpOnly: true,
             // secure: true,
             // sameSite: 'strict',
-            maxAge: 7200000,
+            maxAge: new Date(0),
             path: '/',
           }),
           cookie.serialize('id', result.id, {
             // httpOnly: true,
             // secure: true,
             // sameSite: 'strict',
-            maxAge: 7200000,
+            maxAge: new Date(0),
             path: '/',
           }),
           cookie.serialize('role', result.role, {
             // httpOnly: true,
             // secure: true,
             // sameSite: 'strict',
-            maxAge: 7200000,
+            maxAge: new Date(0),
             path: '/',
           }),
         ]);
+
         res.status(200);
-        res.json({ data: result });
+        res.json({ success: true });
       })
       .catch((error) => {
-        console.log(error, 'error');
+        console.log(error, 'error logout');
       });
   }
 };
-export default login;
+export default logout;
